4c036f6ce7192fc0a11420e8cc4bf76fe9372d91
João Vicente Souto <joaovicentesouto@gmail.com>
2018-05-29 20:12:07 -0300

    Modifies script to describe what you are doing and enter the correct folder

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/zsh
 
-echo "TESTE......"
+echo "Entering INE5418"
+cd INE5418
+
+echo "Creating build folder"
 mkdir build
+
+echo "Entering build folder"
 cd build
+
+echo "Run CMake"
 cmake ..
+
+echo "Run make"
 make
+
+echo "Entering distributed_systems"
 cd distributed_systems
+
+echo "Executes executable"
 ./distributed_systems_executable
