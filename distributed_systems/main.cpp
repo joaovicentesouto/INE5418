24b7d8229d3a8f0fdca1347834472ed4fd3f8f69
João Vicente Souto <joaovicentesouto@gmail.com>
2018-05-29 19:54:24 -0300

    Tests script to automatically use this repo by a Docker container

--- distributed_systems/main.cpp
+++ distributed_systems/main.cpp
@@ -1,11 +1,15 @@
+
 #include <iostream>
+#include <cstdlib>
+
 #include "locks/LocksmithCS.hpp"
 #include "locks/LocksmithMulticast.hpp"
 #include "locks/LocksmithToken.hpp"
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello";
+    if(const char* env_p = std::getenv("HOME"))
+        std::cout << "Your HOME is: " << env_p << '\n';
 
-    return 1;
+    return 0;
 }
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+echo "Seu nome de usuário é:"
+whoami
+echo "Info de hora atual e tempo que o computador está ligado:"
+uptime
+echo "O script está executando do diretório:"
+pwd  
