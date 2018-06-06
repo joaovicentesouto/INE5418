ba4e8b6457cdd6697e2cf8579ce02ae9afcf58b6
João Vicente Souto <joaovicentesouto@gmail.com>
2018-06-06 10:54:01 -0300

    Bug, first key

--- distributed_systems/locks/LocksmithToken.cpp
+++ distributed_systems/locks/LocksmithToken.cpp
@@ -7,27 +7,44 @@ LocksmithToken::LocksmithToken() :
     m_next_name(string_type("container") + std::to_string((std::stoi(std::getenv("ID"))+1) % std::stoi(std::getenv("CONTAINERS_AMOUNT")))),
     m_hostname(string_type("container") + std::getenv("ID"))
 {
+    std::cout << "Create...\n";
     std::thread(&LocksmithToken::ring_algorithm, this).detach();
+    sleep(1);
 }
 
 void LocksmithToken::llock()
 {
+    std::cout << "******** Main lock\n";
     m_mutex.lock();
+    std::cout << "******** Main pegou lock\n";
 }
 
 void LocksmithToken::lunlock()
 {
+    std::cout << "******** Main unlock\n";
     m_mutex.unlock();
+    std::cout << "******** Main saiu\n";
 }
 
 void LocksmithToken::ring_algorithm()
 {
     m_mutex.lock();
+    char message[100];
 
     type::network::io_service udp_server;
-    type::udp::socket_type sock(udp_server, type::ip::udp::v4(), 62000);
 
-    char message[100];
+    type::udp::socket_type sock(udp_server, protocol_type::endpoint(type::ip::udp::v4(), 62000));
+
+    if (m_hostname == "container2")
+    {
+        resolver_type resolver(udp_server);
+
+        auto destiny = *resolver.resolve(query_type(type::ip::udp::v4(), m_next_name, "62000"));
+
+        boost::system::error_code ignored_error;
+
+        sock.send_to(type::network::buffer(message), destiny, 0, ignored_error);
+    }
 
     while (true)
     {
@@ -36,21 +53,18 @@ void LocksmithToken::ring_algorithm()
 
         sock.receive_from(type::network::buffer(message), predecessor, 0, error);
 
-        std::unique_lock<std::mutex> try_lock(m_mutex, std::try_to_lock);
-
-        if (!try_lock.owns_lock())
-        {
-            m_mutex.unlock();
-            m_mutex.lock();
-        }
+        m_mutex.unlock();
+        m_mutex.lock();
 
         strcpy(message, m_hostname.c_str());
 
         resolver_type resolver(udp_server);
+
         auto destiny = *resolver.resolve(query_type(type::ip::udp::v4(), m_next_name, "62000"));
 
 
         boost::system::error_code ignored_error;
+
         sock.send_to(type::network::buffer(message), destiny, 0, ignored_error);
     }
 }
