cb2a771056a41321ba1d8ee4915cd0690ed54e6b
João Vicente Souto <joaovicentesouto@gmail.com>
2018-06-06 11:00:44 -0300

    Token ring implementation
    Using communication via udp running but without any countermeasures
    for eventual death of a container in the queue.

--- distributed_systems/locks/LocksmithToken.cpp
+++ distributed_systems/locks/LocksmithToken.cpp
@@ -7,23 +7,18 @@ LocksmithToken::LocksmithToken() :
     m_next_name(string_type("container") + std::to_string((std::stoi(std::getenv("ID"))+1) % std::stoi(std::getenv("CONTAINERS_AMOUNT")))),
     m_hostname(string_type("container") + std::getenv("ID"))
 {
-    std::cout << "Create...\n";
     std::thread(&LocksmithToken::ring_algorithm, this).detach();
     sleep(1);
 }
 
 void LocksmithToken::llock()
 {
-    std::cout << "******** Main lock\n";
     m_mutex.lock();
-    std::cout << "******** Main pegou lock\n";
 }
 
 void LocksmithToken::lunlock()
 {
-    std::cout << "******** Main unlock\n";
     m_mutex.unlock();
-    std::cout << "******** Main saiu\n";
 }
 
 void LocksmithToken::ring_algorithm()
@@ -32,17 +27,14 @@ void LocksmithToken::ring_algorithm()
     char message[100];
 
     type::network::io_service udp_server;
-
     type::udp::socket_type sock(udp_server, protocol_type::endpoint(type::ip::udp::v4(), 62000));
 
     if (m_hostname == "container2")
     {
         resolver_type resolver(udp_server);
-
         auto destiny = *resolver.resolve(query_type(type::ip::udp::v4(), m_next_name, "62000"));
 
         boost::system::error_code ignored_error;
-
         sock.send_to(type::network::buffer(message), destiny, 0, ignored_error);
     }
 
@@ -59,12 +51,9 @@ void LocksmithToken::ring_algorithm()
         strcpy(message, m_hostname.c_str());
 
         resolver_type resolver(udp_server);
-
         auto destiny = *resolver.resolve(query_type(type::ip::udp::v4(), m_next_name, "62000"));
 
-
         boost::system::error_code ignored_error;
-
         sock.send_to(type::network::buffer(message), destiny, 0, ignored_error);
     }
 }
