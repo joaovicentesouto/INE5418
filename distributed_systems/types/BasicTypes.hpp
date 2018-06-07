0e4fb5ce9cdeafec1f9e261fd1f45b957d07ec8e
João Vicente Souto <joaovicentesouto@gmail.com>
2018-06-07 08:13:50 -0300

    Wrapper folder is no longer needed
    It was just there unused, so I deleted it to make the project
    cleaner.

--- distributed_systems/types/BasicTypes.hpp
+++ distributed_systems/types/BasicTypes.hpp
@@ -13,7 +13,6 @@ namespace type
     using thread_type             = std::thread;
     using mutex_type              = std::mutex;
     using unique_lock_type        = std::unique_lock<mutex_type>;
-    using condition_variable_type = std::condition_variable;
 
     namespace network             = boost::asio;
     namespace ip                  = network::ip;
