919843c32c5d6815e71fad9d5f3f76bf6b54f6d0
João Vicente Souto <joaovicentesouto@gmail.com>
2018-06-08 22:21:48 -0300

    Token ring algorithm execute on tty mode

--- distributed_systems/locks/LocksmithToken.cpp
+++ distributed_systems/locks/LocksmithToken.cpp
@@ -4,9 +4,9 @@ namespace locks
 {
 
 LocksmithToken::LocksmithToken() :
+    m_containers_amount(std::stoi(std::getenv("CONTAINERS_AMOUNT"))),
     m_next_name(string_type("container") + std::to_string((std::stoi(std::getenv("ID"))+1) % std::stoi(std::getenv("CONTAINERS_AMOUNT")))),
-    m_hostname(string_type("container") + std::getenv("ID")),
-    m_containers_amount(std::stoi(std::getenv("CONTAINERS_AMOUNT")))
+    m_hostname(string_type("container") + std::getenv("ID"))
 {
     type::thread_type(&LocksmithToken::ring_algorithm, this).detach();
 }
@@ -33,29 +33,20 @@ void LocksmithToken::ring_algorithm()
     char message[100];
     type::error_type error;
     type::network::io_service udp_server;
+
     type::udp::socket_type socket(udp_server, protocol_type::endpoint(type::ip::udp::v4(), 62000));
 
-    //! Last container starts sending token.
-    if (m_hostname == "container" + std::to_string(m_containers_amount-1))
-    {
-        resolver_type resolver(udp_server);
-        auto destiny = *resolver.resolve(query_type(type::ip::udp::v4(), m_next_name, "62000"));
-
-        std::cout << "xxxxxx " << m_hostname << std::endl << std::flush;
-        socket.send_to(type::network::buffer(message), destiny, 0, error);
-
-        error.clear();
-    }
-
-    type::udp::endpoint_type predecessor;
-    
     resolver_type resolver(udp_server);
-    auto next_address = *resolver.resolve(query_type(type::ip::udp::v4(), m_next_name, "62000"));
+    type::udp::endpoint_type predecessor;
+    type::udp::endpoint_type next_address = *resolver.resolve(query_type(type::ip::udp::v4(), m_next_name, "62000"));
+
+    //! Last container starts sending token.
+    if (m_next_name == "container0")
+        socket.send_to(type::network::buffer(message), next_address, 0, error);
 
     while (true)
     {
         //! Wait token.
-        std::cout << "yyyyyy " << m_hostname << std::endl << std::flush;
         socket.receive_from(type::network::buffer(message), predecessor, 0, error);
 
         //! Releases thread main.
@@ -67,9 +58,6 @@ void LocksmithToken::ring_algorithm()
 
         //! Sends token.
         strcpy(message, m_hostname.c_str());
-        std::cout << "zzzzzz " << m_hostname << std::endl << std::flush;
-        
-        error.clear();
         socket.send_to(type::network::buffer(message), next_address, 0, error);
     }
 }
